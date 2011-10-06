0aa941dfbad678bc7e5e40cb638f4c000789be6a
Parsha Pourkhomami <parshap+git@gmail.com>
2011-10-05 22:06:19 -0700

    Server entry point

---
+++ server.js
@@ -0,0 +1,25 @@
+var connect = require("connect"),
+	browserify = require("browserify"),
+	server = require("./lib/server"),
+
+	httpServer = connect.createServer(),
+	gameServer = new server.Application()
+
+
+// Set up the game server
+;(function() {
+	gameServer.listen(httpServer)
+}())
+
+
+// Set up the http server
+;(function() {
+	httpServer.use(connect.static('public'))
+
+	httpServer.use(browserify({
+		entry: "./client.js",
+		watch: true,
+	}))
+
+	httpServer.listen(8500)
+}())
